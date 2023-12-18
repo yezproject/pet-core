cde3c26826561c5b17018c1deabe9d70fe2db880
Phan Dũng <105138442+phandung2101@users.noreply.github.com>
2023-12-18 22:58:49 +0700

    [CHANGE] update config for infrastructure docker (#13)

--- src/main/java/org/yproject/pet/core/PetCoreApplication.java
+++ src/main/java/org/yproject/pet/core/PetCoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreApplication {
+public class PetCoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CoreApplication.class, args);
+        SpringApplication.run(PetCoreApplication.class, args);
     }
 
 }
