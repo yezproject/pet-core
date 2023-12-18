3d068cb73d047b4f950bf8e4fa520e9210559986
phandung2101 <dungpv.dev@gmail.com>
2023-12-18 21:27:21 +0700

    [CHANGE] remove docker compose and add Dockerfile

--- docker/scripts/init.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS test_table;
-CREATE TABLE test_table
-(
-    id      serial PRIMARY KEY,
-    content VARCHAR(50)
-);
-
-INSERT INTO test_table (content) VALUES ('This is content should be on init database');
